29ed5eccdff065f87b0f8f1b90a5df1184cfa1d1
lx1186541801 <1186541801@qq.com>
2019-04-16 15:49:32 +0800

    切割头部和尾部子视图

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="footer">
+    <img src="https://iocaffcdn.phphub.org/uploads/sites/KDiyAbV0hj1ytHpRTOlVpucbLebonxeX.png" class="brand-icon" alt="">
+    <a href="">
+        刻意联系， 每日精进
+    </a>
+    <div class="float-right">
+        <a href="/about">关于</a>
+    </div>
+</footer>
---
+++ resources/views/layouts/_header.blade.php
@@ -0,0 +1,9 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container">
+        <a href="/" class="navbar-brand">WEIBO App</a>
+        <ul class="navbar-nav justify-content-end">
+            <li class="nav-item"><a href=" {{ route('help') }}">HELP</a></li>
+            <li class="nav-item"><a href=" {{ route('help') }}">LOGIN</a></li>
+        </ul>
+    </div>
+</nav>
