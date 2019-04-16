d99696bf21b4a8a57537cafec398fd72bae6ad22
lx1186541801 <1186541801@qq.com>
2019-04-16 19:12:14 +0800

    登陆时记住我

--- app/Http/Controllers/SessionController.php
+++ app/Http/Controllers/SessionController.php
@@ -21,7 +21,7 @@ class SessionController extends Controller
             'password'  =>  'required'
         ];
         $credentials = $this->validate($request, $rules);
-        if(Auth::attempt($credentials)) {
+        if (Auth::attempt($credentials, $request->has('remember'))) {
             session()->flash('success', '欢迎回来！');
             return redirect()->route('users.show', [Auth::user()]);
         } else {
