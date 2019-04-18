a27c3fb35bd9f47cf3e1ba6723117bd450cca267
lx1186541801 <1186541801@qq.com>
2019-04-18 17:56:37 +0800

    发布微博

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -4,6 +4,23 @@
 
 
 @section('content')
+    @if (Auth::check())
+
+        <div class="row">
+            <div class="col-md-8">
+                <section class="status_form">
+                    @include('shared._status_form')
+                </section>
+            </div>
+        </div>
+
+        <aside class="col-md-4">
+            <section class="user_info">
+                @include('shared._user_info',['user' => Auth::user()] )
+            </section>
+        </aside>
+    @else
+
     <div class="jumbotron">
     <h1>HOME</h1>
         <p class="lead">
@@ -17,4 +34,5 @@
         </p>
 
     </div>
+    @endif
 @stop
--- routes/web.php
+++ routes/web.php
@@ -38,3 +38,7 @@ Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm'
 Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
 Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
 Route::post('password/reset', 'Auth\ResetPasswordController@reset')->name('password.update');
+
+
+// 微博路由 只要store destroy
+Route::resource('statuses', 'StatusesController', ['only' => ['store', 'destroy']]);
