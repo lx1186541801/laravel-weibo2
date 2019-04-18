e080e7e824fa397d974f4bc2a718ab8300e022a8
lx1186541801 <1186541801@qq.com>
2019-04-18 17:03:40 +0800

    配置正式邮箱--移除邮件发送的from 选项

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -108,13 +108,11 @@ class UsersController extends Controller
     {
         $view = 'emails.confirm';
         $data = compact('user');
-        $from = '3193702605@qq.com';
-        $name = 'LJ';
         $to = $user->email;
         $subject = "感谢注册 WEIBO App 应用！ 请确认你的邮箱";
 
-        Mail::send($view, $data, function($message) use ($from, $name, $to, $subject) {
-            $message->from($from, $name)->to($to)->subject($subject);
+        Mail::send($view, $data, function($message) use ($to, $subject) {
+            $message->to($to)->subject($subject);
         });
     }
 
